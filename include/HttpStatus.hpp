de923bd96f891c65acbf06d442b0c6278313583c
erickramosxp <erickramossilva@gmail.com>
2025-10-21 23:06:54 -0300

    :construction: makeResponseStatus for build status mensage

--- include/HttpStatus.hpp
+++ include/HttpStatus.hpp
@@ -9,8 +9,12 @@ class HttpStatus {
     private:
     
         std::map<int, std::string> httpStatusMap;
+        int codeStatus;
     public:
         HttpStatus();
         ~HttpStatus();
         const std::string& getHttpStatusMensager(const int& status) const;
+        int getCodeStatus();
+        void setCodeStatus(const int& codeStatus);
+        const std::string makeResponseStatus();
 };
